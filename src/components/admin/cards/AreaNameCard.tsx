48af3a26a9d8fd82ed89c1c21737912e1f56ef1b
Danmark-Jajang <lightning0145@gmail.com>
2025-05-22 16:02:26 +0900

    Style : 지역명 UI 변경

--- src/components/admin/cards/AreaNameCard.tsx
+++ src/components/admin/cards/AreaNameCard.tsx
@@ -7,11 +7,14 @@ interface AreaNameCardProps {
 
 const AreaNameCard = ({ areaName, areaCode }: AreaNameCardProps) => {
     return (
-        <div className="bg-white p-3 rounded-lg shadow flex items-center h-32">
+        <div className="bg-white p-4 rounded-lg border border-white/20 shadow flex items-center h-32">
             <div className="w-full">
-                <h3 className="font-semibold text-sm text-gray-500 mb-1">
-                    지역명
-                </h3>
+                <div className="flex items-center gap-2 mb-2">
+                    <div className="w-1 h-5 bg-indigo-500 rounded"></div>
+                    <h3 className="font-semibold text-sm text-gray-500">
+                        지역명
+                    </h3>
+                </div>
                 <div className="flex flex-col justify-between">
                     <p className="text-black font-bold text-3xl">{areaName}</p>
                     <p className="text-gray-600 text-sm mt-1">{areaCode}</p>
