deae59920806f4626220ed24a57307546aadba50
iseungho <ghehf51@naver.com>
2025-05-28 13:43:46 +0900

    Design: 챗봇 위치 조절

--- src/components/stars/map/PlaceSuggestion.tsx
+++ src/components/stars/map/PlaceSuggestion.tsx
@@ -17,14 +17,14 @@ export default function PlaceSuggestion() {
     return (
         <div>
             <div
-                className={`absolute bottom-8 transform left-4 z-20 max-w-md bg-white shadow-md flex items-center rounded-full transition-all duration-300 ${
+                className={`absolute bottom-4 transform left-4 z-20 max-w-md bg-white shadow-md flex items-center rounded-full transition-all duration-300 ${
                     isSuggestionOpen
                         ? "bg-opacity-90"
                         : "bg-opacity-60 hover:bg-opacity-90"
-                } md:bottom-8 md:left-6 md:transform-none md:w-88`}
+                } md:bottom-4 md:left-4 md:transform-none md:w-88`}
             >
                 <button
-                    className="flex-shrink-0 bg-transparent mr-3 focus:outline-none border-0
+                    className="flex-shrink-0 px-6 py-3 bg-transparent focus:outline-none border-0
 font-bold text-center text-purple-500"
                     onClick={toggleSuggestion}
                 >
